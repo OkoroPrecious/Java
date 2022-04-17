ef9e7e4c83e73dcc5d6d6833743bf79eac6db962
OkoroPrecious <103753662+OkoroPrecious@users.noreply.github.com>
2022-04-17 01:24:36 +0100

    new

---
+++ HealthApplicationBMI.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+public class HealthApplicationBMI {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+int           = input.next
---
+++ src/chapter2/ConvertPounds.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class ConvertPounds {
+    public static void main(String[] args) {
+        String name = "Precious";
+
+
+        System.out.println(name);
+    }
+}
